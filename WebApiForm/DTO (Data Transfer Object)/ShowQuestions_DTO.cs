7429f8142edb8c63962f9d47fa104e511ac76829
Skillkat <66493830+Skillkat@users.noreply.github.com>
2026-01-28 09:32:39 -0400

    Se agregaron los ID al DTO ShowQuestions_DTO.cs

--- WebApiForm/DTO (Data Transfer Object)/ShowQuestions_DTO.cs
+++ WebApiForm/DTO (Data Transfer Object)/ShowQuestions_DTO.cs
@@ -3,7 +3,9 @@
     public int? IdSesion { get; set; }
     public string TipoRespuesta { get; set; } = null!;
     public string? GrupoTema { get; set; }
+    public string codPregunta { get; set; } = null!;
     public string? Pregunta { get; set; }
+    public string codSubPregunta { get; set; } = null!;
     public string? SubPregunta { get; set; }
     public int Estado { get; set; } 
     public string? Rango { get; set; }
